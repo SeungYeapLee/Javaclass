8e115d2aca9586a5af311a09ddf20778a703271b
SeungYeapLee <38101772+SeungYeapLee@users.noreply.github.com>
2018-04-16 17:56:57 +0000

    Add files via upload

---
+++ Subject1.java
@@ -0,0 +1,31 @@
+
+/**============================================
+ * @FileName: Profile.java
+ * 
+ * @LastModifyDate: 2018.4.6
+ * @LastModifier: LSY
+ * @LastVersion: 1.0
+ * 2018.4.5 LSY 1.0 최초 생성
+ * ============================================*/
+
+/**
+ * 내 프로필 클래스 
+ * 
+ * <p>Copyright 2015 the original author or authors.</p>
+ *
+ * @version 1.0
+ * @author LSY
+ */
+
+public class Subject1 {
+
+	public static void main(String[] args) {
+//		//20180406 프로필 내용 추가함 LSY
+//		System.out.println("기본 사항: LSY, XXXX.XX.XX, 대졸");
+//		System.out.println("특이 사항: 노래듣기, 노래부르기, 고기먹기");
+//		System.out.println("경력 사항: JAVA 배우는 중, TOEIC 시험보기");
+
+	
+	}
+
+}
